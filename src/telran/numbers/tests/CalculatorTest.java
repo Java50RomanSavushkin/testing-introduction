7c32ac265d660ad44dd9cef2480c8cbed5320707
RomanSavushkin <romarsa82@gmail.com>
2023-05-25 23:34:45 +0300

    full CW#1

--- src/telran/numbers/tests/CalculatorTest.java
+++ src/telran/numbers/tests/CalculatorTest.java
@@ -9,19 +9,19 @@ class CalculatorTest {
 
 	@Test
 	void testSum() {
-		assertEquals(4, sum(2,2));
-		assertEquals(0, sum(2,-2));
-		assertEquals(-4, sum(-2,-2));
+		assertEquals(4, sum(2, 2));
+		assertEquals(0, sum(2, -2));
+		assertEquals(-4, sum(-2, -2));
 	}
 
 	@Test
 	void testSubtract() {
-		assertEquals(0, subtract(2,2));
-		assertEquals(4, subtract(2,-2));
-		assertEquals(-2, subtract(2,4));
-		assertEquals(2, subtract(4,2));
-		assertEquals(-2, subtract(-4,-2));
-		assertEquals(2, subtract(-2,-4));
+		assertEquals(0, subtract(2, 2));
+		assertEquals(4, subtract(2, -2));
+		assertEquals(-2, subtract(2, 4));
+		assertEquals(2, subtract(4, 2));
+		assertEquals(-2, subtract(-4, -2));
+		assertEquals(2, subtract(-2, -4));
 	}
 
 	@Test
@@ -38,7 +38,7 @@ class CalculatorTest {
 		assertEquals(0, divide(2, 4));
 		assertEquals(2, divide(4, 2));
 		assertEquals(-2, divide(-4, 2));
-		assertEquals(2, divide(-4, -2));
+		assertEquals(2, divide (-4, -2));
 	}
 
 	@Test
@@ -46,7 +46,7 @@ class CalculatorTest {
 		assertEquals(2, remainder(2, 4));
 		assertEquals(0, remainder(4, 2));
 		assertEquals(-1, remainder(-10, 3));
-		assertEquals(1, remainder(-10, -3));
+		assertEquals(-1, remainder(-10, -3));
 	}
 
 }
